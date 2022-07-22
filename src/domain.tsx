70d6c3aa6ad4da5727270773faafb3b0521ef0da
Alexander Bandukwala <7h3kk1d@gmail.com>
2022-07-21 19:14:52 -0500

    Add Json2 type that is tagged

--- src/domain.tsx
+++ src/domain.tsx
@@ -1,6 +1,50 @@
 type Json = number | string | boolean | null | { [key: string]: Json } | Json[]
 
+type JsonNumber = { kind: "number"; value: number; }
+type JsonString = { kind: "string"; value: string; }
+type JsonBoolean = { kind: "boolean"; value: boolean; }
+type JsonNull = {kind: "null"; }
+type JsonArray = {kind: "array"; value: Json2[]; }
+type JsonObject = {kind: "object"; value: {"key": string, "value": Json2}[];}
 
+
+type Json2 = JsonNumber | JsonString | JsonBoolean | JsonNull | JsonObject | JsonArray
+
+
+function  jsonNull() : JsonNull {
+  return {kind: "null"} 
+}
+function jsonString(value: string) : JsonString{
+  return {kind: "string", value: "string"}
+}
+function jsonBoolean(value: boolean) : JsonBoolean {
+  return {kind: "boolean", value};
+}
+function jsonNumber(value: number) : JsonNumber {
+  return {kind: "number", value};
+}
+function jsonArray(value: Json2[]) : JsonArray {
+  return {kind: "array", value};
+}
+function jsonObject(value: {"key": string, "value": Json2}[]) : JsonObject {
+  return {kind: "object", value: value}
+}
+
+function parse(json: Json) : Json2 {
+  if (json == null) {
+    return jsonNull();
+  } else if (typeof (json) == "string") {
+    return jsonString(json)
+  } else if (typeof (json) == "boolean") {
+    return jsonBoolean(json)
+  } else if (typeof (json) == "number") {
+    return jsonNumber(json)
+  } else if (Array.isArray(json)) {
+    return jsonArray(json.map(parse))
+  } else {
+    return jsonObject(Object.entries(json as object).map(kv => ({ key: kv[0], value: kv[1] })))
+  }
+}
 /* Alternative Approaches:
     Some form of CoFree on Json so I don't have to specify this at all
     Recursive path implementation where the carrier types contain the rest of the path
